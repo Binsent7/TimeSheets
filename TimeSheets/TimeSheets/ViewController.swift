b1545fe5ab60707d6ee6918d741ba30339dbe678
Binsent7 <hishinuma.kougakuin@gmail.com>
2015-09-02 22:04:04 +0900

    プロジェクトファイルを作成

---
+++ TimeSheets/TimeSheets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeSheets
+//
+//  Created by hishinuma on 2015/09/02.
+//  Copyright © 2015年 binsent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
